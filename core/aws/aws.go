2bb402e5fa6c9e67cd77adeaa0e5f7f7ffd4a23d
harry01@github.com <harry1-flo@github.com>
2025-09-29 12:48:47 +0900

    updatE

--- core/aws/aws.go
+++ core/aws/aws.go
@@ -7,15 +7,6 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 )
 
-var DreamsuProfileARNS = map[string]string{
-	"dev":           "972467631093",
-	"dreamusaws001": "294438099013",
-	"data":          "822479450351",
-	"deploy":        "803135791119",
-	"prd":           "865306278000",
-	"sec":           "953265915023",
-}
-
 var NamingConvert = map[string]string{
 	"dev":           "dev-flo",
 	"dreamusaws001": "dreamusaws001",
