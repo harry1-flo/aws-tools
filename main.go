fdb9a6b9200a274140f8d14d08063f04b3de8755
harry01@github.com <harry1-flo@github.com>
2025-10-22 11:24:40 +0900

    update

--- main.go
+++ main.go
@@ -7,17 +7,6 @@ import (
 	"github.com/ldg940804-aws-tools/jobs"
 )
 
-/*
-	var DreamsuProfileARNS = map[string]string{
-	"dev":           "972467631093",
-	"dreamusaws001": "294438099013",
-	"data":          "822479450351",
-	"deploy":        "803135791119",
-	"prd":           "865306278000",
-	"sec":           "953265915023",
-}
-*/
-
 var acs = []string{"dev", "dreamusaws001", "data", "deploy", "prd", "sec"}
 
 func main() {
@@ -28,14 +17,6 @@ func main() {
 
 		// resources
 		ec2 := aws.NewEC2(*cfg, account)
-		// route53 := aws.NewRoute53Config(*cfg, account)
-
-		// jobs
-		// jobs.ListEC2NotAutoScaling(*ec2)
 		jobs.ListEC2(*ec2)
-		// jobs.ListEC2AutoScalingButNotBackupTag(*ec2)
-		// jobs.ListingDNSRecord(*route53)
-
-		// jobs.ISMSEC2(*ec2)
 	}
 }
